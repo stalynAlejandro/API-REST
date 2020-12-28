3964dbb53b23cb4b6cf6e8020b2ed0f9e62737c3
=stalynAlejandro <=saavalencia97@gmail.com>
2020-12-28 23:26:46 +0100

    BoardTime on Mobile :)

---
+++ BoardTime/mobile/src/components/Menu.tsx
@@ -0,0 +1,104 @@
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+} from '@ionic/react';
+
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { bookmarks, bookmarkOutline, person, personOutline } from 'ionicons/icons';
+import './Menu.css';
+
+interface AppPage {
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
+
+const appPages: AppPage[] = [
+  {
+    title: 'Profile',
+    url: '/page/Profile',
+    iosIcon: person,
+    mdIcon: personOutline
+  },
+  {
+    title: 'Tasks',
+    url: '/page/Tasks',
+    iosIcon: bookmarkOutline,
+    mdIcon: bookmarks
+  }
+  // {
+  //   title: 'Favorites',
+  //   url: '/page/Favorites',
+  //   iosIcon: heartOutline,
+  //   mdIcon: heartSharp
+  // },
+  // {
+  //   title: 'Archived',
+  //   url: '/page/Archived',
+  //   iosIcon: archiveOutline,
+  //   mdIcon: archiveSharp
+  // },
+  // {
+  //   title: 'Trash',
+  //   url: '/page/Trash',
+  //   iosIcon: trashOutline,
+  //   mdIcon: trashSharp
+  // },
+  // {
+  //   title: 'Spam',
+  //   url: '/page/Spam',
+  //   iosIcon: warningOutline,
+  //   mdIcon: warningSharp
+  // }
+];
+
+// const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+const Menu: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <IonMenu contentId="main" type="overlay">
+      <IonContent>
+
+        <IonList id="inbox-list">
+          <IonListHeader>BoardTime</IonListHeader>
+          <IonNote>hi@ionicframework.com</IonNote>
+
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                  <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                  <IonLabel>{appPage.title}</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+
+        </IonList>
+
+        {/* <IonList id="labels-list">
+          <IonListHeader>Labels</IonListHeader>
+          {labels.map((label, index) => (
+            <IonItem lines="none" key={index}>
+              <IonIcon slot="start" icon={bookmarkOutline} />
+              <IonLabel>{label}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList> */}
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default Menu;
